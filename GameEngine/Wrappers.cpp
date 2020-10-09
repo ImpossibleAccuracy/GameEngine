fb1a2d5c8faa9f1f3dadd288310e5630f8a0b71a
ImpossibleAccuracy <rvarzin1@gmail.com>
2020-10-09 15:17:20 +0500

    Add branch for C++

---
+++ GameEngine/Wrappers.cpp
@@ -0,0 +1,50 @@
+#include "Wrappers.h"
+
+/*using namespace Wrappers;
+
+void GameComponent_PyWrapper::close() {
+    if (boost::python::override f = this->get_override("close")) {
+        f(); }
+}
+void GameComponent_PyWrapper::loadObjects() {
+    if (boost::python::override f = this->get_override("loadObjects")) {
+        f(); }
+}
+bool GameComponent_PyWrapper::canRender() {
+    if (boost::python::override f = this->get_override("canRender")) {
+        return f(); }
+    return false;
+}
+void GameComponent_PyWrapper::onNotify(GameComponent* sender, boost::shared_ptr<Message> msg) {
+    if (boost::python::override f = this->get_override("onNotify")) {
+        f(sender, msg); }
+}
+void GameComponent_PyWrapper::draw() {
+    if (boost::python::override f = this->get_override("draw")) {
+        f(); }
+}
+void GameComponent_PyWrapper::update() {
+    if (boost::python::override f = this->get_override("update")) {
+        f(); }
+}
+void GameComponent_PyWrapper::onKeyUp(SDL_Keycode key) {
+    if (boost::python::override f = this->get_override("onKeyUp")) {
+        f(key); }
+}
+void GameComponent_PyWrapper::onKeyDown(SDL_Keycode key) {
+    if (boost::python::override f = this->get_override("onKeyDown")) {
+        f(key); }
+}
+void GameComponent_PyWrapper::onMouseUp(int x, int y) {
+    if (boost::python::override f = this->get_override("onMouseUp")) {
+        f(x, y); }
+}
+void GameComponent_PyWrapper::onMouseDown(int x, int y) {
+    if (boost::python::override f = this->get_override("onMouseDown")) {
+        f(x, y); }
+}
+void GameComponent_PyWrapper::onMouseMotion(int x, int y) {
+    if (boost::python::override f = this->get_override("onMouseMotion")) {
+        f(x, y); }
+}*/
+
